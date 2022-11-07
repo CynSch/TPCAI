aae5cde480c4fe9892d2764f7714b641ccc6cdc2
abril bauer <abril_jazmin_bauer@hotmail.com>
2022-11-07 13:05:05 -0300

    Detalles en Region mundial

--- TPCAI/RegionMundial.cs
+++ TPCAI/RegionMundial.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +12,38 @@ namespace TPCAI
         // propiedades 
         public int CodigoDeRegionMundial { get; set; }
         public string NombreDeRegionMundial { get; set; }
-        public  List<Pais> Paises { get; set; }
+        public  List<Pais> PaisesAsociados { get; set; }
+
+        static private List<RegionMundial> LstRegionesMundiales { get; set; }
+
+        //construtor
+        public RegionMundial(int codigoDeRegionMundial, string nombreDeRegionMundial)
+        {
+            CodigoDeRegionMundial = codigoDeRegionMundial;
+            NombreDeRegionMundial = nombreDeRegionMundial;
+           // PaisesAsociados = Pais.
+        }
      
         // Metodos
         public void ListarRegiones()
         {
-            // Devuelve la lista de regiones mundiales
+            //Cargo las regiones mundiales desde el archivo a la lista RegionesMundiales para que esten en memoria
+            var archivoRegionMundial = new StreamReader("TextFile1.txt");
+            while (!archivoRegionMundial.EndOfStream)
+            {
+                string proximaLinea = archivoRegionMundial.ReadLine();
+
+                //Codigo|Nombre
+
+                string[] datosSeparados = proximaLinea.Split('|');
+                RegionMundial regionMundial = new RegionMundial();
+                regionMundial.CodigoDeRegionMundial = int.Parse(datosSeparados[0]);
+                regionMundial.NombreDeRegionMundial = datosSeparados[1];
+
+                RegionMundial.LstRegionesMundiales.Add(regionMundial);
+              
+
+            }
         }
 
     }
