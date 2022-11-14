2ac8133241408025b71553397c1f296ced4cf803
meluverba <melu.verba@gmail.com>
2022-11-13 21:22:01 -0300

    Mensaje de error cuando la factura no tiene orden asociada.

--- TPCAI/CuentaCorrienteServiciosFacturados.cs
+++ TPCAI/CuentaCorrienteServiciosFacturados.cs
@@ -20,41 +20,55 @@ namespace TPCAI
         public void MostrarDatos(string idfacturaseleccionada)
         {
             TxtFactura.Text = idfacturaseleccionada;
+            bool tieneOrdenAsociada = false;
 
             foreach (SolicitudDeOrden orden in SolicitudDeOrden.SolicitudesExistentes)
             {
-                if (orden.CUITCliente == ClienteCorporativo.ClienteActual.CUIT)
+                if (orden.NumeroDeFactura == int.Parse(idfacturaseleccionada))
                 {
-                    if (orden.NumeroDeFactura == int.Parse(idfacturaseleccionada))
+                    tieneOrdenAsociada = true;
+                }
+            }
+
+            if (!tieneOrdenAsociada)
+            {
+                MessageBox.Show("La orden asociada a la factura no se encuentra aún en sistema. Disculpe los inconvenientes, pronto actualizaremos la base de datos con órdenes del pasado.");
+            }
+            else
+            {
+                foreach (SolicitudDeOrden orden in SolicitudDeOrden.SolicitudesExistentes)
+                {
+                    if (orden.CUITCliente == ClienteCorporativo.ClienteActual.CUIT)
                     {
-                        ListViewItem ordenN = new ListViewItem(orden.NumeroDeOrden.ToString()); //nro
-                        ordenN.SubItems.Add(orden.Fecha.ToString()); //fecha
-                        if (Destino.BuscarDestino(orden.NumeroDeOrden).EntregaEnDomicilio == true)
+                        if (orden.NumeroDeFactura == int.Parse(idfacturaseleccionada))
                         {
-                            ordenN.SubItems.Add(Destino.BuscarDestino(orden.NumeroDeOrden).Direccion); //destino
+                            ListViewItem ordenN = new ListViewItem(orden.NumeroDeOrden.ToString()); //nro
+                            ordenN.SubItems.Add(orden.Fecha.ToString()); //fecha
+                            if (Destino.BuscarDestino(orden.NumeroDeOrden).EntregaEnDomicilio == true)
+                            {
+                                ordenN.SubItems.Add(Destino.BuscarDestino(orden.NumeroDeOrden).Direccion); //destino
+                            }
+                            else
+                            {
+                                ordenN.SubItems.Add(Destino.BuscarDestino(orden.NumeroDeOrden).NroSucursal.ToString()); //Sucursal
+                            }
+                            ordenN.SubItems.Add(orden.Importe.ToString());  //monto
+                            lvordenesxfactura.Items.Add(ordenN);
                         }
-                        else
-                        {
-                            ordenN.SubItems.Add(Destino.BuscarDestino(orden.NumeroDeOrden).NroSucursal.ToString()); //Sucursal
-                        }
-                        ordenN.SubItems.Add(orden.Importe.ToString());  //monto
-                        lvordenesxfactura.Items.Add(ordenN);
                     }
                 }
-            }
-            lvordenesxfactura.Sorting = SortOrder.Ascending;
+                lvordenesxfactura.Sorting = SortOrder.Ascending;
 
-            decimal importefactura = 0;
-            foreach (Factura factura in Factura.FacturasExistentes)
-            {
-                if (factura.NroFactura == int.Parse(idfacturaseleccionada))
+                decimal importefactura = 0;
+                foreach (Factura factura in Factura.FacturasExistentes)
                 {
-                    importefactura = factura.Monto;
+                    if (factura.NroFactura == int.Parse(idfacturaseleccionada))
+                    {
+                        importefactura = factura.Monto;
+                    }
                 }
-            }
-            TxtImporteTotal.Text = importefactura.ToString(); 
-
-
+                TxtImporteTotal.Text = importefactura.ToString();
+            } 
         }
 
         private void button1_Click(object sender, EventArgs e)
